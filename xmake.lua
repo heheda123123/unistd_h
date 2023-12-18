495931b73386d407273e53a13365eb2f24100533
xpxz <97490782+heheda123123@users.noreply.github.com>
2023-12-18 22:37:25 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -4,3 +4,6 @@ target("unistd_h")
     set_kind("$(kind)")
     add_files("fork.c")
     add_headerfiles("unistd.h")
+    if is_plat("windows") and is_kind("shared") then
+        add_rules("utils.symbols.export_all", {export_classes = true})
+    end
